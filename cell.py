848ac68875020917289ccb8fc3bbf94d55373a89
shredder42 <atbinder42@gmail.com>
2025-01-29 22:29:28 -0600

    draw_move method

--- cell.py
+++ cell.py
@@ -13,6 +13,8 @@ class Cell():
         self._win = win
 
     def draw(self, x1, y1, x2, y2, fill_color='black'):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -25,6 +27,20 @@ class Cell():
             self._win.draw_line(Line(Point(x1,y1), Point(x2,y1)), fill_color=fill_color)
         if self.has_bottom_wall:
             self._win.draw_line(Line(Point(x1,y2), Point(x2,y2)), fill_color=fill_color)
+
+    def draw_move(self, to_cell, undo=False):
+        center_x = (self._x1 + self._x2) // 2
+        center_y = (self._y1 + self._y2) // 2
+        to_cell_center_x = (to_cell._x1 + to_cell._x2) // 2
+        to_cell_center_y = (to_cell._y1 + to_cell._y2) // 2
+
+        fill_color = 'red'
+        if undo:
+            fill_color = 'gray'
+        
+        line = Line(Point(center_x, center_y), Point(to_cell_center_x, to_cell_center_y))
+        self._win.draw_line(line, fill_color)
+        
 
 
 
--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def main():
     # win.draw_line(Line(Point(50,100), Point(125,200)))
     # win.draw_cell(Cell(200, 200, 300, 300))
     # win.draw_cell(Cell(400, 400, 500, 500, has_left_wall=True, has_right_wall=False, has_top_wall=False))
-    cell = Cell(win)
-    cell.draw(400, 400, 500, 500)
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(300, 300, 500, 500)
+    cell1 = Cell(win)
+    cell1.draw(400, 400, 500, 500)
+    cell2 = Cell(win)
+    cell2.draw(500, 400, 600, 500)
+    cell1.draw_move(cell2, undo=True)
     win.wait_for_close()
 
 
